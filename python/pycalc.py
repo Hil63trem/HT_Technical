81fb6a46c3254a5e07be7af1e4fc20e7c8706411
Hilmar Tremmel <h63t@gmx.de>
2023-03-05 15:30:47 +0100

    Added function add
    File renamed and function added

---
+++ python/pycalc.py
@@ -0,0 +1,10 @@
+def add(x,y):
+    return(x+y)
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print("---------------")
+print(num1, "+", num2, "=", num1 + num2)
+print("____")
+print("HH: ",add(num1,num2))
+print("Feddisch")
